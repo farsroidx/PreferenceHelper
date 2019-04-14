d6bfa3eeee67eed60989d957863535d133ba595f
Mohammad Ali Riazati <47595182+riazati75@users.noreply.github.com>
2019-04-14 13:15:18 +0430

    Initial commit

---
+++ app/src/main/java/ir/farsroidx/MainActivity.java
@@ -0,0 +1,16 @@
+package ir.farsroidx;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SPUtils sp = new SPUtils(this);
+
+    }
+}
---
+++ utils/src/main/java/ir/farsroidx/SPUtils.java
@@ -0,0 +1,69 @@
+package ir.farsroidx;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+public class SPUtils {
+
+    private Context context;
+
+    public SPUtils(Context context) {
+        this.context = context;
+    }
+
+    private SharedPreferences preference() {
+        return PreferenceManager.getDefaultSharedPreferences(context);
+    }
+
+    public String readString(String key, String alternative) {
+        return preference().getString(key, alternative);
+    }
+
+    public float readFloat(String key, float alternative) {
+        return preference().getFloat(key, alternative);
+    }
+
+    public long readLong(String key, long alternative) {
+        return preference().getLong(key, alternative);
+    }
+
+    public int readInteger(String key, int alternative) {
+        return preference().getInt(key, alternative);
+    }
+
+    public boolean readBoolean(String key, boolean alternative) {
+        return preference().getBoolean(key, alternative);
+    }
+
+    public void writeString(String key, String str) {
+        SharedPreferences.Editor editor = preference().edit();
+        editor.putString(key, str);
+        editor.apply();
+    }
+
+    public void writeFloat(String key, float flt) {
+        SharedPreferences.Editor editor = preference().edit();
+        editor.putFloat(key , flt);
+        editor.apply();
+    }
+
+    public void writeLong(String key, long lng) {
+        SharedPreferences.Editor editor = preference().edit();
+        editor.putLong(key,lng);
+        editor.apply();
+    }
+
+    public void writeInteger(String key, int integer) {
+        SharedPreferences.Editor editor = preference().edit();
+        editor.putInt(key, integer);
+        editor.apply();
+    }
+
+    public void writeBoolean(String key, boolean bln) {
+        SharedPreferences.Editor editor = preference().edit();
+        editor.putBoolean(key, bln);
+        editor.apply();
+    }
+
+}
